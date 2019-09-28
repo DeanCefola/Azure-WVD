fd50327ce2dfc947f6458b4ca373daaac33add73
Dean Cefola <deacef@microsoft.com>
2019-09-28 15:58:43 -0400

    WIP update
    update WIP folder for tempalte.
    Add azuredeploy.json for general, simple deployment

--- PowerShell/New-WVDSessionHost.ps1
+++ PowerShell/New-WVDSessionHost.ps1
@@ -72,13 +72,6 @@ Expand-Archive `
     -Force `
     -Verbose
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$AdminsKey = "SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
-$UsersKey = "SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
-$BaseKey = [Microsoft.Win32.RegistryKey]::OpenBaseKey("LocalMachine","Default")
-$SubKey = $BaseKey.OpenSubkey($AdminsKey,$true)
-$SubKey.SetValue("IsInstalled",0,[Microsoft.Win32.RegistryValueKind]::DWORD)
-$SubKey = $BaseKey.OpenSubKey($UsersKey,$true)
-$SubKey.SetValue("IsInstalled",0,[Microsoft.Win32.RegistryValueKind]::DWORD)
 cd $Localpath 
 
 
@@ -97,6 +90,13 @@ If(((Get-CimInstance win32_operatingsystem).Name) -match 'server') {
             -Verbose `
             -LogPath "$Localpath\RdsServerRoleInstall.txt"
     }
+    $AdminsKey = "SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $UsersKey = "SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    $BaseKey = [Microsoft.Win32.RegistryKey]::OpenBaseKey("LocalMachine","Default")
+    $SubKey = $BaseKey.OpenSubkey($AdminsKey,$true)
+    $SubKey.SetValue("IsInstalled",0,[Microsoft.Win32.RegistryValueKind]::DWORD)
+    $SubKey = $BaseKey.OpenSubKey($UsersKey,$true)
+    $SubKey.SetValue("IsInstalled",0,[Microsoft.Win32.RegistryValueKind]::DWORD)
 }
 Else {
     "Windows Client OS Detected"
---
+++ WVDTemplates/WVD-NewHost/bin/Debug/staging/WVD-NewHost/CustomScripts/test.ps1
@@ -0,0 +1,1 @@
+<# Custom Script for Windows #>
