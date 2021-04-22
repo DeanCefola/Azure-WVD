6722865a9f9f13267b1d1a818c3af8b1003ca40c
Dean Cefola <Dean.Cefola@microsoft.com>
2021-04-22 02:37:50 -0400

    update PowerShell Script
    PS script update for AZ-140 Series

--- PowerShell/FSLogixSetup.ps1
+++ PowerShell/FSLogixSetup.ps1
@@ -17,6 +17,77 @@ Param (
         [string]$ProfilePath
 )
 
+######################
+#    WVD Variables   #
+######################
+$LocalWVDpath            = "c:\temp\wvd\"
+$FSLogixURI              = 'https://aka.ms/fslogix_download'
+$FSInstaller             = 'FSLogixAppsSetup.zip'
+
+
+####################################
+#    Test/Create Temp Directory    #
+####################################
+if((Test-Path c:\temp) -eq $false) {
+    Add-Content -LiteralPath C:\New-WVDSessionHost.log "Create C:\temp Directory"
+    Write-Host `
+        -ForegroundColor Cyan `
+        -BackgroundColor Black `
+        "creating temp directory"
+    New-Item -Path c:\temp -ItemType Directory
+}
+else {
+    Add-Content -LiteralPath C:\New-WVDSessionHost.log "C:\temp Already Exists"
+    Write-Host `
+        -ForegroundColor Yellow `
+        -BackgroundColor Black `
+        "temp directory already exists"
+}
+if((Test-Path $LocalWVDpath) -eq $false) {
+    Add-Content -LiteralPath C:\New-WVDSessionHost.log "Create C:\temp\WVD Directory"
+    Write-Host `
+        -ForegroundColor Cyan `
+        -BackgroundColor Black `
+        "creating c:\temp\wvd directory"
+    New-Item -Path $LocalWVDpath -ItemType Directory
+}
+else {
+    Add-Content -LiteralPath C:\New-WVDSessionHost.log "C:\temp\WVD Already Exists"
+    Write-Host `
+        -ForegroundColor Yellow `
+        -BackgroundColor Black `
+        "c:\temp\wvd directory already exists"
+}
+New-Item -Path c:\ -Name New-WVDSessionHost.log -ItemType File
+Add-Content `
+-LiteralPath C:\New-WVDSessionHost.log `
+"
+ProfilePath       = $ProfilePath
+RegistrationToken = $RegistrationToken
+Optimize          = $Optimize
+"
+
+
+#################################
+#    Download WVD Componants    #
+#################################
+Add-Content -LiteralPath C:\New-WVDSessionHost.log "Downloading FSLogix"
+    Invoke-WebRequest -Uri $FSLogixURI -OutFile "$LocalWVDpath$FSInstaller"
+
+
+##############################
+#    Prep for WVD Install    #
+##############################
+Add-Content -LiteralPath C:\New-WVDSessionHost.log "Unzip FSLogix"
+Expand-Archive `
+    -LiteralPath "C:\temp\wvd\$FSInstaller" `
+    -DestinationPath "$LocalWVDpath\FSLogix" `
+    -Force `
+    -Verbose
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+cd $LocalWVDpath 
+Add-Content -LiteralPath C:\New-WVDSessionHost.log "UnZip FXLogix Complete"
+
 
 #########################
 #    FSLogix Install    #
@@ -51,8 +122,8 @@ Set-ItemProperty `
     -Value "1"
 New-ItemProperty `
     -Path HKLM:\Software\FSLogix\Profiles `
-    -Name "VHDLocations" `
-    -Value $ProfilePath `
+    -Name "CCDLocations" `
+    -Value "type=smb,connectionString=$ProfilePath" `
     -PropertyType MultiString `
     -Force
 Set-ItemProperty `
