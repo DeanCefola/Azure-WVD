04863252c18b50c03b2c0f99573b461c3c0bb1a0
Dean Cefola <Dean.Cefola@microsoft.com>
2021-10-13 18:33:23 -0400

    Add logging

--- PowerShell/RenameComputer.ps1
+++ PowerShell/RenameComputer.ps1
@@ -9,13 +9,36 @@ Param (
     $VMName
 )
 
-Begin
-{
-    $CurrentName = $env:COMPUTERNAME
+Begin {    
+####################################
+#    Test/Create Temp Directory    #
+####################################
+if((Test-Path c:\temp) -eq $false) {
+    Add-Content -LiteralPath C:\RenameComputer.log "Create C:\temp Directory"
+    Write-Host `
+        -ForegroundColor Cyan `
+        -BackgroundColor Black `
+        "creating temp directory"
+    New-Item -Path c:\temp -ItemType Directory
+}
+else {
+    Add-Content -LiteralPath C:\RenameComputer.log "C:\temp Already Exists"
+    Write-Host `
+        -ForegroundColor Yellow `
+        -BackgroundColor Black `
+        "temp directory already exists"
+}
+New-Item -Path c:\ -Name C:\RenameComputer.log -ItemType File
+Add-Content -LiteralPath C:\RenameComputer.log `
+"
+CurrentName     = $env:COMPUTERNAME
+TargetName      = $VMName
+"
+
 }
 
-Process
-{
+Process {
+    Add-Content -LiteralPath C:\RenameComputer.log "Current ComputerName = $env:COMPUTERNAME, Rename Target = $VMName"
     Rename-Computer -NewName $VMName -Restart -Verbose
 }
 
