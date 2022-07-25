80d4b6ee28202016b017cd99321f0f2f3e69cc17
Dean Cefola <Dean.Cefola@microsoft.com>
2022-07-25 11:41:10 -0400

    Create ___AVD_Get App Group Assignment.ps1
    find users assigned to app groups

---
+++ PowerShell/___AVD_Get App Group Assignment.ps1
@@ -0,0 +1,33 @@
+﻿<#Author       : Dean Cefola
+# Creation Date: 07-09-2020
+# Usage        : Get AVD App Group Assignment
+# 
+#********************************************************************************
+# Date                         Version      Changes
+#------------------------------------------------------------------------
+# 07/09/2020                     1.0        Intial Version
+#
+
+#*********************************************************************************
+#
+#>
+###################
+#    Variables    #
+###################
+$RGName = 'GPU'
+$AppGroupName = 'HP-GPU-DAG'
+$AVDGroup = Get-AzWvdApplicationGroup `
+    -Name $AppGroupName `
+    -ResourceGroupName $RGName
+
+
+#################################
+#    Get AVD Role Assignment    #
+#################################
+Get-AzRoleAssignment `
+    -Scope $AVDGroup.Id | `
+    Where-Object `
+        -Property RoleDefinitionName `
+        -match 'Desktop Virtualization User'
+
+
